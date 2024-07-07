aa33ce9f24b3e7dd8cc594075a102a722a369138
unknown <prashantpattanashetti5@gmail.com>
2024-07-07 23:46:02 +0530

    Primitive and Hello world using IntelliJIDE community version

---
+++ Java/First-idea-program/first-idea-program/src/com/kunal/Primitives.java
@@ -0,0 +1,14 @@
+package com.kunal;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo  =64; //size 4 bytes
+        char letter ='r';
+        float marks =98.97f; // 4 bytes
+        double largeDecimalNumbers = 454545.67412; // 8 bytes
+        long largeInteger =8574512421541L; // 8 bytes
+        boolean check =  false;
+
+        
+    }
+}
