67498bf23a8a59fd13e8fa7bc3c566ae42a712d4
unknown <prashantpattanashetti5@gmail.com>
2024-07-12 12:38:03 +0530

    Some modifications

--- Java/First-idea-program/first-idea-program/src/com/kunal/Inputs.java
+++ Java/First-idea-program/first-idea-program/src/com/kunal/Inputs.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("please enter some input: ");
         int rollNo = input.nextInt();
         System.out.println("Your Roll no is : "+rollNo);
 
