e209af68d3f37a856f607e670018aba1b039defe
unknown <prashantpattanashetti5@gmail.com>
2024-07-12 12:56:04 +0530

    user inputs and values using primitive data type

--- Java/First-idea-program/first-idea-program/src/com/kunal/Inputs.java
+++ Java/First-idea-program/first-idea-program/src/com/kunal/Inputs.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("please enter some input: ");
-        int rollNo = input.nextInt();
-        System.out.println("Your Roll no is : "+rollNo);
+//        System.out.print("please enter some input: ");
+//        int rollNo = input.nextInt();
+//        System.out.println("Your Roll no is : "+rollNo);
 
+        String name = input.next();
+        System.out.println(name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
     }
 }
