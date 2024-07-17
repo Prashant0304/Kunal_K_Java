754c376f809f4bebd88d99888b4ec061112c1fc2
unknown <prashantpattanashetti5@gmail.com>
2024-07-17 12:07:45 +0530

    case check problem

---
+++ Java/conditions-loops/code/src/com/kunal/CaseCheck.java
@@ -0,0 +1,9 @@
+package com.kunal;
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char name = in.next().trim().charAt(0);
+        System.out.println(name);
+    }
+}
